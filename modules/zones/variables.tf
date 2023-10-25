d42769450fc309d3f38a64952513028d4201e578
Sebastian Rivera Soto <115038803+sriveraxepelin@users.noreply.github.com>
2023-10-25 12:25:28 -0600

    Update variables.tf

--- modules/zones/variables.tf
+++ modules/zones/variables.tf
@@ -11,7 +11,36 @@ variable "zones" {
 }
 
 variable "tags" {
-  description = "Tags added to all zones. Will take precedence over tags from the 'zones' variable"
-  type        = map(any)
-  default     = {}
+  type = object({
+    name           = string
+    owner          = string
+    environment    = string
+    team           = string
+    repo           = string
+    provision      = string
+    ticket         = string
+  })
+  description = <<-EOT
+  name:
+    Default name for resources if it was not provided at resource-level.
+
+  owner:
+    Functional team accountable for resources created.
+
+  environment:
+    Environment resources are being created at.
+    Example values: sandbox, dev, qa, prod, homo
+
+  team:
+    team to which resources created correspond to.
+
+  repo:
+    Github repository
+  
+  provision:
+    Provision method. (terraform)
+  
+  ticket:
+    Jira ticket link. (N/A default)
+  EOT
 }
